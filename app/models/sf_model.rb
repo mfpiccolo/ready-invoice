6256cacff322b108755604ea6f5086999ea3a8ee
Mike Piccolo <mfpiccolo@gmail.com>
2013-12-04 14:11:14 -0800

    Add logic to allow SfJson to act like AR

--- app/models/sf_model.rb
+++ app/models/sf_model.rb
@@ -1,5 +1,27 @@
 class SfModel < ActiveRecord::Base
-  # will have @record and @type
+  belongs_to :salesforce
 
-  # will overwrite the getter method for records to use json_to_ar
+  after_initialize :setup_model
+
+  def all
+    records[model_name].map { |record| JsonToAR.new(record) }
+  end
+
+  def find( param )
+    all.detect { |i| i.Id == param } || raise(ActiveRecord::RecordNotFound)
+  end
+
+  def where( query )
+    ret = []
+    all.each { |i| ret.push(i) if i.include?(query) } ||
+        raise(ActiveRecord::RecordNotFound)
+    return ret
+  end
+
+
+  private
+
+  def setup_model
+    SF.const_set(model_name, self)
+  end
 end
