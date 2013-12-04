358c139f541d4ed713b44c5b85682f57afd1a321
Mike Piccolo <mfpiccolo@gmail.com>
2013-12-04 14:09:50 -0800

    Add refinements keep model small

--- app/models/json_to_ar.rb
+++ app/models/json_to_ar.rb
@@ -1,34 +1,6 @@
 class JsonToAR
-  attr_accessor :json
 
-  #replaced with dynamic initialization below
-  #attr_accessor :id, :user, :state
-  #
-  # Sample JSON
-  #
-  # { "id":1, "user":"john", "state":"active" }
-
-
-  def initialize(json)
-    @json = json
-  end
-
-  # def self.all
-  #   json.map { |i| convert(i) }
-  # end
-
-  # def self.find( param )
-  #   all.detect { |i| i.id == param } || raise(ActiveRecord::RecordNotFound)
-  # end
-
-  # def self.where( query )
-  #   ret = []
-  #   all.each { |i| ret.push(i) if i.include?(query) } ||
-  #       raise(ActiveRecord::RecordNotFound)
-  #   return ret
-  # end
-
-  def convert item
+  def initialize item
     raise "expected Hash param" unless item.kind_of? Hash
     item.each do |key,value|
       instance_variable_set("@#{key.to_s}".to_sym, value)
