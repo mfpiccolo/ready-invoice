a65e54b1972bec99ac8b2d53c115240039ee4e0d
Mike Piccolo <mfpiccolo@gmail.com>
2013-12-11 16:55:46 -0800

    Update client based on user credentials

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,6 @@
 class Client
   def self.create(opts={}, env=Rails.env)
-    case opts[:client]
+    case opts[:wrapper]
     when :databasedotcom
       cfg = YAML.load_file(Rails.root.join("config/databasedotcom.yml"))[env]
       Databasedotcom::Client.new(cfg).tap do |client|
@@ -8,25 +8,19 @@ class Client
       end
     when :restforce
       cfg = YAML.load_file(Rails.root.join("config/restforce.yml"))[env]
-      Metaforce.new username: cfg["username"],
-        password:        cfg["password"],
-        security_token:  cfg["security_token"],
+      Metaforce.new username: opts[:sf_credentials][:username],
+        password:        opts[:sf_credentials][:password],
+        security_token:  opts[:sf_credentials][:security_token],
         client_id:       cfg["client_id"],
         client_secret:   cfg["client_secret"]
     when :metaforce
       cfg = YAML.load_file(Rails.root.join("config/metaforce.yml"))[env]
-      Metaforce.new username: cfg["username"],
-        password: cfg["password"],
-        security_token: cfg["security_token"]
-    when :bulk_api
-      cfg = YAML.load_file(Rails.root.join("config/bulk_api.yml"))[env]
-      SalesforceBulkApi::Api.new({
-        client_id: cfg["client_id"],
-        client_secret: cfg["client_secret"],
-        host: cfg["host_name"],
-        username: cfg["username"],
-        password: ["password"]
-      })
+      Metaforce.new username: opts[:sf_credentials][:username],
+        password: opts[:sf_credentials][:password],
+        security_token: opts[:sf_credentials][:security_token]
+    when :salesforce_bulk
+      cfg = YAML.load_file(Rails.root.join("config/salesforce_bulk.yml"))[env]
+      SalesforceBulk::Api.new(opts[:sf_credentials][:username], (opts[:sf_credentials][:password] + opts[:sf_credentials][:security_token]))
     end
   end
 end
