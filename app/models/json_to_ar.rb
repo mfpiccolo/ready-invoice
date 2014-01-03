1607f2ac0dde86b6f2a26e4ce718ce14b861677f
Mike Piccolo <mfpiccolo@gmail.com>
2014-01-03 10:25:53 -0800

    override inspect for JsonToAR objects.

--- app/models/json_to_ar.rb
+++ app/models/json_to_ar.rb
@@ -1,6 +1,11 @@
 class JsonToAR
 
-  def initialize item
+  attr_reader :item, :model_name
+
+  def initialize item, model_name
+    @item = item
+    @model_name = model_name
+
     raise "expected Hash param" unless item.kind_of? Hash
     item.each do |key,value|
       instance_variable_set("@#{key.to_s}".to_sym, value)
@@ -24,4 +29,12 @@ class JsonToAR
     "https://localhost:3001/invoices/#{self.to_param}"
   end
 
+  def inspect
+    attributes_as_nice_string = self.instance_variables.collect { |name|
+      unless name.to_s == "@item" || name.to_s == "@model_name"
+        "#{name.to_s}: #{self.send(name.to_s.gsub("@", "").to_sym)}"
+      end
+    }.compact.join(", ")
+    "#<#{model_name} #{attributes_as_nice_string}>"
+  end
 end
