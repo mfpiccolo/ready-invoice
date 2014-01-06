628cd94c831df1376086285b7ec4f56f026a020e
Mike Piccolo <mfpiccolo@gmail.com>
2014-01-06 10:52:49 -0800

    Annotate model

--- app/models/sf_object_relation.rb
+++ app/models/sf_object_relation.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: sf_object_relations
+#
+#  id          :integer          not null, primary key
+#  parent_id   :integer
+#  parent_type :string(255)
+#  child_id    :integer
+#  child_type  :string(255)
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class SfObjectRelation < ActiveRecord::Base
   belongs_to :parent, class_name: "SfObject"
   belongs_to :child, class_name: "SfObject"
