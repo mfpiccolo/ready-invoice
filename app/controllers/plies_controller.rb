37b77c2ace35c5a188ef46ce7fd1e9f38ad87f2f
Mike Piccolo <mfpiccolo@gmail.com>
2014-01-25 06:02:10 -0800

    Add flash messages

--- app/controllers/plies_controller.rb
+++ app/controllers/plies_controller.rb
@@ -1,7 +1,8 @@
 class PliesController < ApplicationController
 
   def update_all
-    SfSynch.(current_user)
+    # SfSynch.(current_user)
+    flash_message :notice, 'This is working!'
     redirect_to "/"
   end
 end
