c68f72b91541d71bc5467f12a0ead439d28cfed8
Mike Piccolo <mfpiccolo@gmail.com>
2014-01-25 04:58:30 -0800

    Serve static assets

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # Environment variables (ENV['...']) are set in the file config/application.yml.
 # See http://railsapps.github.com/rails-environment-variables.html
-puts 'ROLES'
-YAML.load(ENV['ROLES']).each do |role|
-  Role.find_or_create_by_name({ :name => role }, :without_protection => true)
-  puts 'role: ' << role
-end
+# puts 'ROLES'
+# YAML.load(ENV['ROLES']).each do |role|
+#   Role.find_or_create_by_name({ :name => role }, :without_protection => true)
+#   puts 'role: ' << role
+# end
