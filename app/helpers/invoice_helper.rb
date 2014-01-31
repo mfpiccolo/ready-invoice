356d7e54e9fcaec5afb4c3f6c2907300ef6b178f
Mike Piccolo <mfpiccolo@gmail.com>
2014-01-30 23:37:05 -0800

    Pass user_id to helper

--- app/helpers/invoice_helper.rb
+++ app/helpers/invoice_helper.rb
@@ -1,14 +1,15 @@
 module InvoiceHelper
 
-  def layout_value(id, position)
+  def layout_value(id, position, user_id)
     li = LineItem.find(id)
+    user = User.find(user_id)
 
-    if li.otype == current_user.layout.send("li_#{position}_model")
-      li.send(current_user.layout.send("li_#{position}_attribute"))
+    if li.otype == user.layout.send("li_#{position}_model")
+      li.send(user.layout.send("li_#{position}_attribute"))
     else
-      model = li.children.where(otype: current_user.layout.send("li_#{position}_model")).first
-      if model.send(current_user.layout.send("li_#{position}_attribute")).present?
-         model.send(current_user.layout.send("li_#{position}_attribute"))
+      model = li.children.where(otype: user.layout.send("li_#{position}_model")).first
+      if model.send(user.layout.send("li_#{position}_attribute")).present?
+         model.send(user.layout.send("li_#{position}_attribute"))
       else
         ""
       end
