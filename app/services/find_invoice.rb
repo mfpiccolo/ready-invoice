c63498eaac3b965ff1902223d4211af3cf95e763
Mike Piccolo <mfpiccolo@gmail.com>
2014-01-31 08:50:32 -0800

    Remove puts statements

--- app/services/find_invoice.rb
+++ app/services/find_invoice.rb
@@ -4,8 +4,6 @@ class FindInvoice
   collaborators :invoice_oid, :user_id
 
   def call(&block)
-    puts "User ID: #{user_id}"
-    puts "Invoice ID: #{invoice_oid}"
     Invoice.where(user_id: user_id, oid: invoice_oid).first
     # user.invoices.find_by_oid(invoice_oid)
   end
