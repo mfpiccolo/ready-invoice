3ddf55172630a61719c40195baf7550838267e0d
Mike Piccolo <mfpiccolo@gmail.com>
2014-02-07 20:44:31 -0800

    Remove unused block arg

--- app/services/find_invoice.rb
+++ app/services/find_invoice.rb
@@ -3,7 +3,7 @@ class FindInvoice
 
   collaborators :invoice_oid, :user_id
 
-  def call(&block)
+  def call
     Invoice.where(user_id: user_id, oid: invoice_oid).first
     # user.invoices.find_by_oid(invoice_oid)
   end
