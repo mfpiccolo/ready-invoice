df2fcb9b5cff008461f667b01d4661d307756995
Mike Piccolo <mfpiccolo@gmail.com>
2014-02-17 20:04:33 -0800

    Add controller test for stripe action.

--- test/controllers/subscriptions_controller_test.rb
+++ test/controllers/subscriptions_controller_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 describe SubscriptionsController do
 
   before do
-    @subscription = subscriptions(:one)
+    @subscription = FactoryGirl.create(:subscription)
   end
 
   it "must get index" do
@@ -17,12 +17,40 @@ describe SubscriptionsController do
     assert_response :success
   end
 
-  it "must create subscription" do
-    assert_difference('Subscription.count') do
-      post :create, subscription: {  }
+  describe "POST create" do
+    before do
+      @user = FactoryGirl.create(:user)
+      @customer = OpenStruct.new(id: "abc123")
+    end
+    describe "with current_user subscription present" do
+      before do
+        @subscription = @user.create_subscription(stripe_cus_id: "abc123")
+        SubscriptionsController.any_instance.stubs(:current_user).returns(@user)
+        @subscription.expects(:update_attributes).with(:email => 'blah@blah.com', :plan_id => '1', :stripe_token => 'l0ng_t0k3n')
+        Stripe::Customer.expects(:retrieve).with("abc123").returns(@customer)
+        @customer.expects(:save)
+      end
+
+      it "updates subscription attributes locally and for stripe" do
+        post :create, email: "blah@blah.com", plan_id: 1, stripeToken: "l0ng_t0k3n"
+      end
     end
 
-    assert_redirected_to subscription_path(assigns(:subscription))
+    describe "without current_user subscription present" do
+      before do
+        @subscription = @user
+        Stripe::Customer.expects(:create)
+          .with(:card => 'l0ng_t0k3n', :plan => '1', :email => 'blah@blah.com')
+          .returns(@customer)
+        SubscriptionsController.any_instance.stubs(:current_user).returns(@user)
+        @user.expects(:create_subscription)
+          .with(:email => 'blah@blah.com', :plan_id => '1', :stripe_token => 'l0ng_t0k3n', :stripe_cus_id => "abc123")
+      end
+
+      it "creates a new customer and associates them with a subscription plan" do
+        post :create, email: "blah@blah.com", plan_id: 1, stripeToken: "l0ng_t0k3n"
+      end
+    end
   end
 
   it "must show subscription" do
